3be93b28a320b19e0ac7335a71d3f3ba4e60cb96
IgorM213 <igor.mijanovic02@gmail.com>
2025-08-02 04:17:04 +0200

    Pocetak voze
    konacno funkcionalna voznja 😭

--- Assets/Scripts/PomeranjePoSplineu.cs
+++ Assets/Scripts/PomeranjePoSplineu.cs
@@ -5,64 +5,29 @@ using UnityEngine.Splines;
 public class PomeranjePoSplineu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public SplineContainer spline;
-    public float speed = 3f;
-    float distancePercentage = 0f;
-
     private Rigidbody rb;
 
-    private float movementX;
-    private float movementY;
     [SerializeField] private float power;
+    //public float Speed = 5f;
 
-    float splineLength;
 
     private void Start()
     {
-        splineLength = spline.CalculateLength();
         rb = GetComponent<Rigidbody>();
     }
-    void OnMove(InputValue movementValue)
+
+    private void FixedUpdate()
     {
-        // Convert the input value into a Vector2 for movement.
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        // Store the X and Y components of the movement.
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        distancePercentage += speed * Time.deltaTime / splineLength;
-
-        Vector3 currentPosition = spline.EvaluatePosition(distancePercentage);
-        transform.position = currentPosition;
-
-        if (distancePercentage > 1f)
+        if (Input.GetKey(KeyCode.W))
         {
-            distancePercentage = 0f;
+            Throttle(power);
         }
 
-        //Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-        Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
-        Vector3 direction = nextPosition - currentPosition;
-        transform.rotation = Quaternion.LookRotation(direction, transform.up);
+        if (Input.GetKey(KeyCode.S))
+        {
+            Throttle(-power);
+        }
     }
-    //private void FixedUpdate()
-    //{
-    //    if (Input.GetKey(KeyCode.W))
-    //    {
-    //        Throttle(power);
-    //    }
-
-    //    if (Input.GetKey(KeyCode.S))
-    //    {
-    //        Throttle(-power);
-    //    }
-    //}
 
     private void Throttle(float power)
     {
---
+++ Assets/Scripts/Sine.cs
@@ -0,0 +1,86 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Splines;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Sine : MonoBehaviour
+{
+    public SplineContainer splineContainer;
+    public float moveSpeed = 1f;
+    [SerializeField] private float power;
+    private Rigidbody rb;
+
+    private float velocity = 0f;
+
+    [SerializeField] private float acceleration = 0.05f;
+    [SerializeField] private float deceleration = 0.001f;
+    [SerializeField] private float brakingForce = 0.01f;
+    [SerializeField] private float maxSpeed = 2f;
+
+
+    private float currentSplinePosition = 0f;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (splineContainer == null)
+        {
+            Debug.Log("zavsrio se");
+
+            return;
+        }
+
+        // Ako držiš W ? ubrzaj
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += acceleration/2 * Time.deltaTime;
+        }
+
+        // Ako držiš S ? ko?i
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (velocity > 0f)
+            {
+                velocity -= brakingForce/2 * Time.deltaTime;
+            }
+        }
+        // Ako ništa ne držiš ? prirodno usporavaj (trenje)
+        else
+        {
+            if (velocity > 0)
+                velocity -= deceleration/3 * Time.deltaTime;
+            else if (velocity < 0)
+                velocity += deceleration/3 * Time.deltaTime;
+        }
+
+        // Ograni?i brzinu
+        velocity = Mathf.Clamp(velocity, -maxSpeed, maxSpeed);
+
+        // Pomeri voz po spline-u
+        currentSplinePosition += velocity * Time.deltaTime / splineContainer.Splines[0].GetLength();
+        currentSplinePosition = (currentSplinePosition + 1f) % 1f;
+
+
+
+        // Get position and tangent (direction) from the spline
+        Vector3 position = splineContainer.EvaluatePosition(currentSplinePosition);
+        //Debug.Log(position);
+
+        Vector3 tangent = splineContainer.EvaluateTangent(currentSplinePosition);
+
+        // Update player's position and rotation
+        transform.position = position;
+        transform.rotation = Quaternion.LookRotation(tangent);
+    }
+
+    private void Throttle(float power)
+    {
+        Vector3 dir = power * transform.forward;
+        rb.AddForce(dir);
+    }
+}
